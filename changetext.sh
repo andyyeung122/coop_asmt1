f3ae38030d104610aead73e5e4482fe32319d247
Andy Yeung <andyyeung122@gmail.com>
2019-02-02 15:34:31 -0500

    added text files

--- changetext.sh
+++ changetext.sh
@@ -2,6 +2,18 @@
 for f in *.txt; do
 	declare -i x
 	x=$(wc -l <"$f")
-
+	if [ $x -wq 0 ]
+	then
+		rm -- "$f"
+        elif [ $x -lt 10 ] && [ $x -gt 0 ]
+        then
+                mv -- "$f" "short_$f"
+        elif [ $x -lt 21 ] && [ $x -gt 9 ]
+        then
+                mv -- "$f" "medium_$f"
+        elif [ $x -gt 20 ]
+        then
+                mv -- "$f" "long_$f"
+        fi
 done
 
